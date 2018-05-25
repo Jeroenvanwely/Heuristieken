47e2848b58ecc53b83642e2107a0f3cd721e8b08
Mellaba <melaniebaaten@gmail.com>
2018-05-25 16:46:01 +0200

    little change

---
+++ Code/hillclimber.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+import numpy as np
+import random
+import classes as cc
+import random
+import copy
+
+if __name__ == "__main__":
+    Fold = cc.Fold()
+    Fold.hillclimber()
